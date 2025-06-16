0e4f31a0ad6785c8992bba5dce13c5a4c4a1f2c9
Sowndhar S (MS/ECQ2-XC) <S.Sowndhar@in.bosch.com>
2025-06-16 18:51:24 +0530

    Initial commit of Budget Buddy expense tracker

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increased from default 500kb to 1000kb
+  }
 })
